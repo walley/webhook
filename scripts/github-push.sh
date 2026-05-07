d1ec3af2296b92ab98e58181e6fe8b6a246c4762
walley <walley@walley.org>
2026-05-07 12:18:48 +0200

    script

--- scripts/github-push.sh
+++ scripts/github-push.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-echo pushie pooshie
+date > /tmp/syncresult
 
+echo pushie pooshie, sleeping for a bit
+#sleep 10
 
-#echo "Received push event. Updating local repository..." >> /var/log/webhook.log
-#cd /path/to/your/repo && git pull origin main
+logger -p local6.info "running hook"
+
+cd /var/www/git/intranet
+pwd  >> /tmp/syncresult
+
+/usr/bin/git pull 2>&1 >> /tmp/syncresult
+
+rsync -avv --exclude '.git/' /var/www/git/intranet/ /var/www/html/test/intranet/ 2>&1 >> /tmp/syncresult
